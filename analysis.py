47cfe99dd35002a2b5f76b8bc77ead21c1f8ab68
bit <kickscar@gmail.com>
2018-06-25 11:14:50 +0900

    바그래프 그리기

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # pelicana
 pelicana_table = pd.DataFrame.from_csv(
@@ -9,7 +10,7 @@ pelicana_table = pd.DataFrame.from_csv(
 
 pelicana_table = pelicana_table[pelicana_table.sido != '']
 pelicana_table = pelicana_table[pelicana_table.gungu != '']
-pelicana = pelicana_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis=1).value_counts() # 'SIDO GUNGU' 별 매장수
+pelicana = pelicana_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis='columns').value_counts() # 'SIDO GUNGU' 별 매장수
 # print(pelicana)
 
 # nene
@@ -21,7 +22,7 @@ nene_table = pd.DataFrame.from_csv(
 
 nene_table = nene_table[nene_table.sido != '']
 nene_table = nene_table[nene_table.gungu != '']
-nene = nene_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis=1).value_counts() # 'SIDO GUNGU' 별 매장수
+nene = nene_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis='columns').value_counts() # 'SIDO GUNGU' 별 매장수
 # print(nene)
 
 # kyochon
@@ -33,7 +34,7 @@ kyochon_table = pd.DataFrame.from_csv(
 
 kyochon_table = kyochon_table[kyochon_table.sido != '']
 kyochon_table = kyochon_table[kyochon_table.gungu != '']
-kyochon = kyochon_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis=1).value_counts() # 'SIDO GUNGU' 별 매장수
+kyochon = kyochon_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis='columns').value_counts() # 'SIDO GUNGU' 별 매장수
 # print(kyochon)
 
 # goobne
@@ -45,5 +46,14 @@ goobne_table = pd.DataFrame.from_csv(
 
 goobne_table = goobne_table[goobne_table.sido != '']
 goobne_table = goobne_table[goobne_table.gungu != '']
-goobne = goobne_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis=1).value_counts() # 'SIDO GUNGU' 별 매장수
-print(goobne)
+goobne = goobne_table.apply(lambda r: str(r['sido']) + ' ' + str(r['gungu']), axis='columns').value_counts() # 'SIDO GUNGU' 별 매장수
+# print(goobne)
+
+chicken_table = pd.DataFrame({'pelicana': pelicana, 'nene': nene, 'kyochon': kyochon, 'goobne': goobne}).fillna(0)
+chicken_table = chicken_table.drop(chicken_table[chicken_table.index == '00 18'].index)
+chicken_table = chicken_table.drop(chicken_table[chicken_table.index == '테스트 테스트구'].index)
+chicken_sum_table = chicken_table.sum(axis=0)
+
+plt.figure()
+chicken_sum_table.plot(kind='bar')
+plt.show()
